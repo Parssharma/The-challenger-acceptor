a6d1cf783181c7da14cdfbc0ec52b79291fafdea
Parshant Sharma <parsharma900@gmail.com>
2026-09-04 00:49:36 +0530

    Time: 6 ms (52.36%), Space: 165.8 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+            int min_odd=INT_MAX;
+            int min_even=INT_MAX;
+            
+        for(int i=0;i<nums1.size();i++){
+            if(nums1[i]%2!=0 ){
+                min_odd= min(min_odd,nums1[i]);
+            }
+            else{
+                 min_even=min(min_even,nums1[i]);
+            }
+        }
+            if((min_even-min_odd)>=1){
+                return true;
+            }
+        if(min_odd==INT_MAX||min_even==INT_MAX){
+            return true;
+        }
+        return false;
+    }
+};
