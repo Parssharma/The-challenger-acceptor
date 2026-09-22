69e19018c64098fe1a825350548ca3bfaad3b7a9
Parshant Sharma <parsharma900@gmail.com>
2026-09-23 01:27:11 +0530

    Time: 0 ms (100%), Space: 23.1 MB (20.04%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,43 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    int height(TreeNode* root){
-        if(root==nullptr){
-            return 0;
-        }
-
-        int left=height(root->left);
-        int right=height(root->right);
-
-        int ans=max(left,right)+1;
-        return ans;
-
-    }
 public:
+    pair<bool,int> isBalancedfast(TreeNode* root) {
+        if(root == nullptr) {
+            return make_pair(true, 0);
+        }
+
+        pair<bool,int> left = isBalancedfast(root->left);
+        pair<bool,int> right = isBalancedfast(root->right);
+
+        bool leftans = left.first;
+        bool rightans = right.first;
+
+        bool diff = abs(left.second - right.second) <= 1;
+
+        pair<bool,int> ans;
+
+        ans.second = max(left.second, right.second) + 1;
+
+        if(leftans && rightans && diff) {
+            ans.first = true;
+        }
+        else {
+            ans.first = false;
+        }
+
+        return ans;
+    }
+
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr){
-            return true;
-        }
-        
-        bool left =  isBalanced(root->left);
-        bool right =  isBalanced(root->right);
-
-        bool diff = abs(height(root->left)-height(root->right))<=1;
-
-        if(left && right && diff){
-            return 1;
-        }else {
-            return false;
-        }
-
+        return isBalancedfast(root).first;
     }
 };
